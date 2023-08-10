41f8826a9455308c407f9d6377f6708df7e9fd96
apuu03 <57383340+apuu03@users.noreply.github.com>
2023-08-10 20:01:51 -0300

    primer commit

---
+++ pruebas.js
@@ -0,0 +1,9 @@
+function VentanaAleatoria() {
+    iAleatorio = Math.floor(Math.random() * 23);
+
+    urlAleatoria = "./src/pages/Index (" + iAleatorio + ").html"
+
+    window.open(urlAleatoria, "_blank");
+  }
+
+
---
+++ src/pages/Easter egg.js
@@ -0,0 +1,7 @@
+var imagen = document.getElementById("lunita");
+
+imagen.addEventListener("click", function () {
+  alert(
+    "Esta carta teletransporta al jugador a la habitación inicial del piso actual"
+  );
+});
